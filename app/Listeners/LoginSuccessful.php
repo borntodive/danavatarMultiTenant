06806c338c8376d8d1f7cb858ad1f8c1cbf31b98
andrea <andrea.covelli@gmail.com>
2021-02-03 22:59:33 +0100

    Test Login

--- app/Listeners/LoginSuccessful.php
+++ app/Listeners/LoginSuccessful.php
@@ -27,7 +27,6 @@ class LoginSuccessful
         if (isset($matches[1]))
             $subdomain=rtrim($matches[1], " \t.");
         if ($subdomain) {
-            dd($subdomain);
             $center=Tenant::where(['url'=>$subdomain])->firstOrFail();
             session(['tenant_id' => $center->id]);
             session(['tenant_slug' => $center->slug]);
