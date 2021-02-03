7650a979f4df99abf647faa265ef0493fda8ecd3
andrea <andrea.covelli@gmail.com>
2021-02-03 22:55:57 +0100

    Test Login

--- app/Listeners/LoginSuccessful.php
+++ app/Listeners/LoginSuccessful.php
@@ -21,8 +21,7 @@ class LoginSuccessful
     public function handle(Login $event)
     {
         $url=$this->request->server->all()['HTTP_ORIGIN'];
-        dd($url);
-        preg_match('/^([a-z0-9|-]+[a-z0-9]{1,}\.)*[a-z0-9|-]+[a-z0-9]{1,}\.[a-z]{2,}$/', $_SERVER['SERVER_NAME'], $matches);
+        preg_match('/^([a-z0-9|-]+[a-z0-9]{1,}\.)*[a-z0-9|-]+[a-z0-9]{1,}\.[a-z]{2,}$/',$url, $matches);
         $subdomain=null;
         if (isset($matches[1]))
             $subdomain=rtrim($matches[1], " \t.");
