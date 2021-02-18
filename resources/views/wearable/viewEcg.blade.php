9463c6135f0ad884932ff9d7851495bad979beb4
andre <andrea.covelli@gmail.com>
2021-02-18 18:20:44 +0100

    Ecg timepicker

--- resources/views/wearable/viewEcg.blade.php
+++ resources/views/wearable/viewEcg.blade.php
@@ -15,6 +15,96 @@
         :user="$user"
         :button2="$button2"
     />
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="mt-5">
+        <h2 class="text-gray-500 text-xs font-medium uppercase tracking-wide">Pinned Projects</h2>
+        <ul class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            <li class="col-span-1 flex shadow-sm rounded-md">
+                <div class="flex-shrink-0 flex items-center justify-center w-16 bg-pink-600 text-white text-sm font-medium rounded-l-md">
+                    GA
+                </div>
+                <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                    <div class="flex-1 px-4 py-2 text-sm">
+                        <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Scegli orario</a>
+                        <x-form.text-input id="timepicker"/>
+                    </div>
+                    <div class="flex-shrink-0 pr-2">
+                        <button class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <span class="sr-only">Open options</span>
+                            <!-- Heroicon name: solid/dots-vertical -->
+                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </li>
+
+            <li class="col-span-1 flex shadow-sm rounded-md">
+                <div class="flex-shrink-0 flex items-center justify-center w-16 bg-purple-600 text-white text-sm font-medium rounded-l-md">
+                    CD
+                </div>
+                <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                    <div class="flex-1 px-4 py-2 text-sm truncate">
+                        <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Orario Caricato</a>
+                        <p class="text-gray-500" id="time_loaded"></p>
+                    </div>
+                    <div class="flex-shrink-0 pr-2">
+                        <button class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <span class="sr-only">Open options</span>
+                            <!-- Heroicon name: solid/dots-vertical -->
+                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </li>
+
+            <li class="col-span-1 flex shadow-sm rounded-md">
+                <div class="flex-shrink-0 flex items-center justify-center w-16 bg-yellow-500 text-white text-sm font-medium rounded-l-md">
+                    T
+                </div>
+                <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                    <div class="flex-1 px-4 py-2 text-sm truncate">
+                        <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Primo Dato</a>
+                        <p class="text-gray-500">{{isset($availablesDate['first']->time) ? $availablesDate['first']->time->timezone('Europe/Rome')->toDateTimeString() : ""}}</p>
+                    </div>
+                    <div class="flex-shrink-0 pr-2">
+                        <button class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <span class="sr-only">Open options</span>
+                            <!-- Heroicon name: solid/dots-vertical -->
+                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </li>
+
+            <li class="col-span-1 flex shadow-sm rounded-md">
+                <div class="flex-shrink-0 flex items-center justify-center w-16 bg-green-500 text-white text-sm font-medium rounded-l-md">
+                    RC
+                </div>
+                <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                    <div class="flex-1 px-4 py-2 text-sm truncate">
+                        <a href="#" class="text-gray-900 font-medium hover:text-gray-600">Ultimo Dato</a>
+                        <p class="text-gray-500">{{isset($availablesDate['last']->time) ? $availablesDate['last']->time->timezone('Europe/Rome')->toDateTimeString() : ""}}</p>
+                    </div>
+                    <div class="flex-shrink-0 pr-2">
+                        <button class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <span class="sr-only">Open options</span>
+                            <!-- Heroicon name: solid/dots-vertical -->
+                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+
     <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
         <x-card title="ECG" class="mb-5 h-px-600">
             <div class="highCharts h-96 w-full" id="chart_ecg" ></div>
@@ -22,6 +112,7 @@
         </x-card>
     </div>
 </x-app-layout>
+<link rel="stylesheet" href="https://unpkg.com/@dmuy/timepicker@2.0.0/dist/mdtimepicker.css">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="{{asset('assets/js/vendor/sweetalert2.min.js')}}"></script>
 <script src="{{asset('assets/js/vendor/spin.min.js')}}"></script>
@@ -46,6 +137,7 @@
     var ecgChart;
 
     const timezone = 'Europe/Rome';
+    moment.tz.setDefault(timezone);
     const currentDate = moment("{{ $date }}");
 
     var noData=[];
@@ -182,7 +274,7 @@
     }
 
     function showLoadedTime(startDate) {
-        const loadedDate = moment(startDate + 'Z');
+        const loadedDate = moment(startDate);
         if (loadedDate.format('YYYY-MM-DD HH:mm') != searchTime)
             $('#time_loaded_card').addClass('background-red');
         else
