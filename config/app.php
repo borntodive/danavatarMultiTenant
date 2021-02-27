1b0f022e4481d52ce3e40c2fcd7c849cc4cea61e
andre <andrea.covelli@gmail.com>
2021-02-27 09:14:39 +0100

    remote logging

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Rollbar\Laravel\RollbarServiceProvider::class,
 
         /*
          * Application Service Providers...
