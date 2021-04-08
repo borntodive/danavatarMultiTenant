8d21d5503f46239c831bf992de0dd69af17892b4
Andrea <andrea.covelli@gmail.com>
2021-04-08 18:04:04 +0200

    ecg calc influx

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -569,10 +569,9 @@ class SampleController extends Controller
         $pyOut = exec("python3 ../app/Python/ecgPoints.py -s $sampleRate 2>&1");
 
         //$pyOut=trim($pyOut, '"');
-        dd($pyOut);
         $rawEcgPoints=json_decode($pyOut);
         $rawEcgPoints=json_decode($rawEcgPoints);
-        //dd($rawEcgPoints);
+        dd($rawEcgPoints);
 
         $out=null;
         $startTime=Carbon::createFromTimestampMs($request->startTime);
