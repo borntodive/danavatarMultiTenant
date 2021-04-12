228cb0cff4aa6e230b04205820edcece5dc41dae
Andrea <andrea.covelli@gmail.com>
2021-04-12 18:23:07 +0200

    Revert "Create distribuited hypertables in staging & prod"
    This reverts commit 6c6b9f73

--- database/migrations/2021_02_17_174939_create_samples_hypertable.php
+++ database/migrations/2021_02_17_174939_create_samples_hypertable.php
@@ -9,11 +9,11 @@ class CreateSamplesHypertable extends Migration
     public function up()
     {
         DB::statement('CREATE EXTENSION IF NOT EXISTS timescaledb;');
-        if (app()->environment('local', 'staging'))
+        if (app()->environment('local'))
             DB::statement("SELECT create_hypertable('samples', 'time');");
         else {
             DB::statement("SELECT create_distributed_hypertable('samples', 'time', 'sensor_id');");
-            DB::statement("SELECT add_dimension('samples', 'user_id');");
+            DB::statement("SELECT add_dimension('samples', 'user_id',number_partitions => 2);");
         }
     }
 
