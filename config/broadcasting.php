f7e15358b3d7380a9aa5d33603893cefe840486d
Andrea <andrea.covelli@gmail.com>
2022-01-11 09:06:36 +0100

    fix pusher 2

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,14 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
+                'encrypted' => false,
                 'host' => env('PUSHER_APP_URL'),
-                'port' => 6001,
-                'scheme' => 'http'
+                'port' => 6002,
+                'scheme' => 'https',
+                'curl_options' => [
+                        CURLOPT_SSL_VERIFYHOST => 0,
+                        CURLOPT_SSL_VERIFYPEER => 0
+                ]
             ],
         ],
 
