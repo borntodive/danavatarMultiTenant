5bf3c6be00291dbe486d68d004e4c0730ee4c7c3
Andrea <andrea.covelli@gmail.com>
2022-02-01 10:08:47 +0100

    fix message sentry

--- app/Http/Controllers/Api/SampleController.php
+++ app/Http/Controllers/Api/SampleController.php
@@ -153,7 +153,7 @@ class SampleController extends Controller
         # You can generate a Token from the "Tokens Tab" in the UI
 
         ini_set('max_execution_time', 0);
-        \Sentry\captureMessage(json_encode($request));
+        \Sentry\captureMessage('new dive');
         $validator = Validator::make($this->toSnakeCase($request->all()), [
             'data' => 'required|array'
 
