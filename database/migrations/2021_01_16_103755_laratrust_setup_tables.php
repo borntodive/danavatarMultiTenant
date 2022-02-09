852a9672562e0ec985e7d6845bd82acd95c1949c
Shift <shift@laravelshift.com>
2022-02-09 09:37:34 +0000

    Adopt anonymous migrations

--- database/migrations/2021_01_16_103755_laratrust_setup_tables.php
+++ database/migrations/2021_01_16_103755_laratrust_setup_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class LaratrustSetupTables extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -98,4 +98,4 @@ class LaratrustSetupTables extends Migration
         Schema::dropIfExists('roles');
         Schema::dropIfExists('teams');
     }
-}
+};
--- database/migrations/2021_02_17_065105_create_alerts_table.php
+++ database/migrations/2021_02_17_065105_create_alerts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAlertsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -20,4 +20,4 @@ class CreateAlertsTable extends Migration
     {
         Schema::dropIfExists('alerts');
     }
-}
+};
