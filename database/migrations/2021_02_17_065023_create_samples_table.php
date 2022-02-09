72a864803c7e26747240f5a47fcb7944287db6e5
Shift <shift@laravelshift.com>
2022-02-09 09:37:34 +0000

    Adopt anonymous migrations

--- database/migrations/2021_02_17_065023_create_samples_table.php
+++ database/migrations/2021_02_17_065023_create_samples_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSamplesTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -23,4 +23,4 @@ class CreateSamplesTable extends Migration
     {
         Schema::dropIfExists('samples');
     }
-}
+};
--- database/migrations/2021_12_29_173323_create_new_dive_table.php
+++ database/migrations/2021_12_29_173323_create_new_dive_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNewDiveTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -45,4 +45,4 @@ class CreateNewDiveTable extends Migration
     {
         Schema::dropIfExists('dives');
     }
-}
+};
